a28b79e5a8efc45eacc076cd2c81d8113ef4f8e8
valadzko <pavel.valadzko@gmail.com>
2015-12-17 15:03:17 +0300

    Fix tabs

--- part3/railway_station.rb
+++ part3/railway_station.rb
@@ -1,15 +1,15 @@
 class RailwayStation
   attr_reader :name
-  
-  def initialize(name)
-		@name = name
-    @trains = []
-	end
 
-	def accept_train(train)
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def accept_train(train)
     puts "Train #{train.id} has arrived to the station #{@name}"
-		@trains << train
-	end
+    @trains << train
+  end
 
   def send_train(train_to_send)
     @trains.each do |train|
@@ -20,30 +20,30 @@ class RailwayStation
     end
   end
 
-	def show_trains
-		if @trains.any?
-			@trains.each { |train| puts "Train at the station #{@name}: #{train.id} "}
-		else
-			puts "The are no trains at the #{@name} station at the moment"
+  def show_trains
+    if @trains.any?
+      @trains.each { |train| puts "Train at the station #{@name}: #{train.id} "}
+    else
+      puts "The are no trains at the #{@name} station at the moment"
     end
-	end
+  end
 
   def show_freight_trains
-		if @trains.any?
+    if @trains.any?
       @trains.each do |train|
         next unless train.type == "freight"
         puts "Freight train at the station #{@name} : #{train.id}"
       end
     end
-	end
+  end
 
-	def show_passenger_trains
+  def show_passenger_trains
     if @trains.any?
       @trains.each do |train|
         next unless train.type == "passenger"
         puts "Passenger train at the station #{@name} : #{train.id}"
       end
     end
-	end
+  end
 
 end
