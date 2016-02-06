db7ef661e189bc25816b2d631e3810dd9c6b494a
valadzko <pavel.valadzko@gmail.com>
2016-02-07 01:20:12 +0300

    Add Validate module. Part 9/2.

---
+++ part9/validation.rb
@@ -0,0 +1,44 @@
+module Validation
+
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.include(InstanceMethods)
+  end
+
+  module InstanceMethods
+    def validate!
+      to_validate = []
+      self.methods.each {|m| to_validate << m if m.to_s.start_with?("validation_field_")}
+      to_validate.each {|validation| self.send(validation)}
+    end
+
+    def valid?
+      self.validate!
+      true
+    rescue
+      false
+    end
+
+  end
+
+  module ClassMethods
+    def validate (field, *args)
+
+      case args[0].to_sym
+      when :type
+        define_method("validation_field_#{field}".to_sym) do
+          raise "TypeDoesNotMatch" unless "@#{field.class}" == "#{args[1]}"
+        end
+      when :format
+        define_method("validation_field_#{field}".to_sym) do
+          raise "FailedFormatField" unless "@#{field.class}" =~ "#{args[1]}"
+        end
+      when :presence
+        define_method("validation_field_#{field}".to_sym) do
+          raise "FailedValidatePresenceField" if "@#{field}".nil? || "@#{field}".empty?
+        end
+      end # case-when
+    end # validate
+  end # ClassMethods
+
+end
