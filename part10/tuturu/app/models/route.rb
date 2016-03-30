b7d2b654545a6903a0e2817e208653dddd10220d
valadzko <pavel.valadzko@gmail.com>
2016-03-30 18:50:43 +0300

    Changed sorting railway_stations to use scope

--- part10/tuturu/app/models/route.rb
+++ part10/tuturu/app/models/route.rb
@@ -5,12 +5,6 @@ class Route < ActiveRecord::Base
 
   validates :name, presence: true
 
-  def sorted_stations
-    RailwayStation.joins(:railway_station_routes)
-    .where(["railway_station_routes.route_id=?", self])
-    .order("railway_station_routes.station_index_in_route")
-  end
-
   def update_stations_order(order)
     order.each do |station_id, new_index|
       station = railway_stations.where(id: station_id).first
